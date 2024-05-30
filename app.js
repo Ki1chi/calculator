30e93fc8bca37b57fff4dbbcef9cd0acc1cfbf88
Nori Terada <81499937+Ki1chi@users.noreply.github.com>
2024-05-30 04:22:38 -0600

    Fixed app

--- app.js
+++ app.js
@@ -1,28 +1,80 @@
 const screenNumber = document.querySelector(".calculator-screen")
-const add = document.querySelector("#add")
-const subtract = document.querySelector("#subtract")
-const multiply = document.querySelector("#multiply")
-const divide = document.querySelector("#divide")
+const numbers = document.querySelectorAll(".number")
+const operators = document.querySelectorAll(".operator")
+const smallScreen = document.querySelector(".small-screen")
+const clear = document.querySelector(".all-clear")
+// Displays numbers of the buttons that were pushed
+
+
+numbers.forEach(button => button.addEventListener("click", ()=>{
+    if(screenNumber.value == total){
+        screenNumber.value ="";
+    }
+    screenNumber.value += button.value
+}))
+
+
+
+let total = 0;
+let oldOperator;
+let firstNumber = 1;
+
+
+operators.forEach(button => button.addEventListener("click", ()=>{
+    if(firstNumber == false){
+        if(oldOperator == "-"){
+            total = subtract(total,screenNumber.value);
+        }
+        if(oldOperator == "+"){
+            total = add(total,screenNumber.value);
+        }
+        if(oldOperator == "*"){
+            total = multiply(total,screenNumber.value);
+        }
+        if(oldOperator == "/"){
+            total = divide(total,screenNumber.value);
+        }
+
+    screenNumber.value = total 
+    oldOperator = button.value
+
+    smallScreen.value = `${total} ${button.value}`
+    }
+
+    if (firstNumber == true){
+        total = screenNumber.value;
+        firstNumber = 0;
+        oldOperator = button.value;
+        
+        smallScreen.value = `${total} ${oldOperator}`
+        screenNumber.value = total;
+    }
+}))
+
+
+
+clear.addEventListener("click",()=>{
+    screenNumber.value ="0";
+    smallScreen.value ="";
+    oldOperator ="";
+    total = 0;
+    firstNumber = 1;
+})
 
 
 
 
-
-
-function add (num1,num2){
-    return num1 + num2
+function add (a,b){
+    return (+a + +b)
 }
-function subtract (num1,num2){
-    return num1 - num2
+function subtract (a,b){
+    return (a - b)
 }
-function multiply (num1,num2){
-    return num1 * num2
+function multiply (a,b){
+    return (a * b)
 }
-function divide (num1,num2){
-    return num1 / num2
+function divide (a,b){
+    return (a / b)
 }
 
-function operate(operator){
-    return operator
-}
 
